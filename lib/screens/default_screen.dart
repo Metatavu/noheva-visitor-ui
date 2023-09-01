8b964ecfeeffd788f08a10db5c63b132a494dc5d
Ville Juutila <ville.juutila@gmail.com>
2023-09-01 11:33:42 +0300

    Added display text of approval status

--- lib/screens/default_screen.dart
+++ lib/screens/default_screen.dart
@@ -1,4 +1,9 @@
+import "dart:async";
+
 import "package:flutter/material.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:noheva_visitor_ui/database/dao/keys_dao.dart";
+import "package:simple_logger/simple_logger.dart";
 
 class DefaultScreen extends StatefulWidget {
   const DefaultScreen({super.key});
@@ -8,16 +13,39 @@ class DefaultScreen extends StatefulWidget {
 }
 
 class _DefaultScreenState extends State<DefaultScreen> {
+  bool _isDeviceApproved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    keysDao.checkIsDeviceApproved().then(
+      (value) {
+        if (!value) {
+          Timer.periodic(const Duration(seconds: 5), (timer) async {
+            SimpleLogger().info("Checking whether device has been approved...");
+            bool isApproved = await keysDao.checkIsDeviceApproved();
+            setState(() => _isDeviceApproved = isApproved);
+            if (isApproved) {
+              SimpleLogger().info("Device is approved!");
+              timer.cancel();
+            }
+          });
+        }
+        setState(() => _isDeviceApproved = value);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Default screen",
-            ),
+          children: [
+            Text(_isDeviceApproved
+                ? AppLocalizations.of(context)!.deviceIsApproved
+                : AppLocalizations.of(context)!.deviceNotYetApproved),
           ],
         ),
       ),
