76bf9de729e20af34de61722b6b4b8fb9251301e
Antti Leppä <antti.leppa@metatavu.fi>
2023-10-30 19:48:47 +0200

    Added sparkle

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import flutter_sparkle
 import package_info_plus
 import path_provider_foundation
 import sqlite3_flutter_libs
@@ -13,6 +14,7 @@ import video_player_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterSparklePlugin.register(with: registry.registrar(forPlugin: "FlutterSparklePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
