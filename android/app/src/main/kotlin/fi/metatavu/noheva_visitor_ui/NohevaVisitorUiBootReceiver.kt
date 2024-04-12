34257c7012d39dc70b607dd0d18f37eb0c531123
Ville Juutila <ville.juutila@gmail.com>
2024-04-12 08:28:23 +0300

    Initial support for Android auto start

---
+++ android/app/src/main/kotlin/fi/metatavu/noheva_visitor_ui/NohevaVisitorUiBootReceiver.kt
@@ -0,0 +1,21 @@
+package fi.metatavu.noheva_visitor_ui;
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+/**
+ * Broadcast receiver that starts main activity when device boots
+ */
+class NohevaVisitorUiBootReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d(javaClass.name, "Received boot completed intent!")
+            val mainActivityIntent = Intent(context, MainActivity::class.java)
+            mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            Log.d(javaClass.name, "Starting main activity...")
+            context.startActivity(mainActivityIntent)
+        }
+    }
+}
